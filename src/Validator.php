9097c9258d31618641131b9546129bb2ccc1f42b
michaelyousrie <michaelyousrie@gmail.com>
2025-05-01 07:15:39 +0400

    Update validation rules regex patterns to be better and more accurate. Add specific named methods to the Validation class to make the code more fluent and readable.

--- src/Validator.php
+++ src/Validator.php
@@ -11,12 +11,12 @@ abstract class Validator
         'int'       => '\d+',
         'string'    => '(?=.*[a-zA-Z])[a-zA-Z0-9\-_\.@]+',
         'bool'      => '(true|false)',
-        'hex'       => '[0-9a-fA-F]+',
+        'hex'       => '([0-9a-fA-F]{3}|[0-9a-fA-F]{6})',
         'slug'      => '[a-zA-Z0-9\-]+',
         'ip'        => '(\d{1,3}\.){3}\d{1,3}|[0-9a-fA-F:]+',
         'ipv4'      => '(\d{1,3}\.){3}\d{1,3}',
         'ipv6'      => '[0-9a-fA-F:]+',
-        'date'      => '\d{4}-\d{2}-\d{2}',
+        'date'      => '\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])',
         'uuid'      => '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}',
     ];
 
@@ -60,6 +60,95 @@ abstract class Validator
         }
 
         return (bool) preg_match('/^(' . self::$rules[$rule] . ')$/', $data);
+    }
+
+    /**
+     * @param string|int $data
+     * @return bool
+     */
+    public static function isInt(string|int $data): bool
+    {
+        if (is_int($data)) {
+            return true;
+        }
+
+        return self::validate($data, 'int');
+    }
+
+    /**
+     * @param string|bool $data
+     * @return bool
+     */
+    public static function isBool(string|bool $data): bool
+    {
+        if (is_bool($data)) {
+            return true;
+        }
+
+        return self::validate(strtolower($data), 'bool');
+    }
+
+    /**
+     * @param string $data
+     * @return bool
+     */
+    public static function isIp(string $data): bool
+    {
+        return self::validate($data, 'ip');
+    }
+
+    /**
+     * @param string $data
+     * @return bool
+     */
+    public static function isIpv4(string $data): bool
+    {
+        return self::validate($data, 'ipv4');
+    }
+
+    /**
+     * @param string $data
+     * @return bool
+     */
+    public static function isIpv6(string $data): bool
+    {
+        return self::validate($data, 'ipv6');
+    }
+
+    /**
+     * @param string $data
+     * @return bool
+     */
+    public static function isHex(string $data): bool
+    {
+        return self::validate($data, 'hex');
+    }
+
+    /**
+     * @param string $data
+     * @return bool
+     */
+    public static function isSlug(string $data): bool
+    {
+        return self::validate($data, 'slug');
+    }
+
+    /**
+     * @param string $data
+     * @return bool
+     */
+    public static function isDate(string $data): bool
+    {
+        return self::validate($data, 'date');
+    }
+
+    /**
+     * @param string $data
+     * @return bool
+     */
+    public static function isUuid(string $data): bool
+    {
+        return self::validate($data, 'uuid');
     }
 
     //  =========================== INTERNAL METHODS ===========================
